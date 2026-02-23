c992b794cf74730ad9648331ad0ab79f7c41a3cf
Marhi03 <marhi.patel@gmail.com>
2026-02-23 12:50:34 +0530

    Add files via upload

---
+++ Dict-1.py
@@ -0,0 +1,5 @@
+d1 = {1:'Hello', 2:'World'}
+d2 = {10:'Computer', 20:'Lab'}
+d3 = {100:'Dictionary', 200:'Codes'}
+d = {**d1, **d2, **d3}
+print(d)
---
+++ Dict-5.py
@@ -0,0 +1,4 @@
+d1 = {'oil':2700, 'rice':300, 'dal':125}
+d2 = {'oil':2, 'rice':3, 'dal':4}
+cost = d1['rice']*d2['rice'] + d1['oil']*d2['oil'] + d1['dal']*d2['dal']
+print(cost)
