db08b9981d3821e001a2b89cc43c7a689f5ba5b6
Marhi03 <marhi.patel@gmail.com>
2026-03-04 18:18:11 +0530

    Update Tuple-3.py

--- Tuple-3.py
+++ Tuple-3.py
@@ -1,54 +1,24 @@
 #Suppose a date is represented as a tuple(d,m,y).
 #Create two date tuples and find the number of days between the two dates.
 
-tpl1=()
-tpl2=()
-print('Enter date in dd,mm,yyyy format')
+from datetime import date
 
-for i in range(0,3):
-    n=int(input('\nEnter value for 1st date: '))
-    tpl1=tpl1+(n,)
-for i in range(0,3):
-    n=int(input('\nEnter value for 2nd date: '))
-    tpl2=tpl2+(n,)
+tpl1 = ()
+tpl2 = ()
 
-print('\n1. It is not a leap year')
-print('2. It is a leap year')
-y1=int(input('Enter value for 1st date: '))
-y2=int(input('Enter value for 2nd date: '))
+print("Enter date in dd, mm, yyyy format")
 
-print('\n1. Month has 30 days')
-print('2. Month has 31 days')
-print('3. Month has 28 days')
-m1=int(input('Enter value for 1st date: '))
-m2=int(input('Enter value for 2nd date: '))
+for i in range(3):
+    n = int(input("Enter value for 1st date: "))
+    tpl1 = tpl1 + (n,)
 
-if y1==1:
-    if m1==m2==1:
-        d=((tpl2[2]*365) + (tpl2[1]*30) + tpl2[0]) - ((tpl1[2]*365) + (tpl1[1]*30) + tpl1[0])
-    elif m1==m2==2:
-        d=((tpl2[2]*365) + (tpl2[1]*31) + tpl2[0]) - ((tpl1[2]*365) + (tpl1[1]*31) + tpl1[0])
-    elif m1==m2==3:
-        d=((tpl2[2]*365) + (tpl2[1]*28) + tpl2[0]) - ((tpl1[2]*365) + (tpl1[1]*28) + tpl1[0])
-    elif m1==1 and m2==2:
-        d=((tpl2[2]*365) + (tpl2[1]*30) + tpl2[0]) - ((tpl1[2]*365) + (tpl1[1]*31) + tpl1[0])
-    elif m1==2 and m2==3:
-        d=((tpl2[2]*365) + (tpl2[1]*31) + tpl2[0]) - ((tpl1[2]*365) + (tpl1[1]*28) + tpl1[0])
-    elif m1==1 and m2==3:
-        d=((tpl2[2]*365) + (tpl2[1]*30) + tpl2[0]) - ((tpl1[2]*365) + (tpl1[1]*28) + tpl1[0])
+for i in range(3):
+    n = int(input("Enter value for 2nd date: "))
+    tpl2 = tpl2 + (n,)
 
-if y2==2:
-    if m2==m1==1:
-        d=((tpl2[2]*366) + (tpl2[1]*30) + tpl2[0]) - ((tpl1[2]*366) + (tpl1[1]*30) + tpl1[0])
-    elif m2==m1==2:
-        d=((tpl2[2]*366) + (tpl2[1]*31) + tpl2[0]) - ((tpl1[2]*366) + (tpl1[1]*31) + tpl1[0])
-    elif m2==m1==3:
-        d=((tpl2[2]*366) + (tpl2[1]*28) + tpl2[0]) - ((tpl1[2]*366) + (tpl1[1]*28) + tpl1[0])
-    elif m1==1 and m2==2:
-        d=((tpl2[2]*366) + (tpl2[1]*30) + tpl2[0]) - ((tpl1[2]*366) + (tpl1[1]*31) + tpl1[0])
-    elif m1==2 and m2==3:
-        d=((tpl2[2]*366) + (tpl2[1]*31) + tpl2[0]) - ((tpl1[2]*366) + (tpl1[1]*28) + tpl1[0])
-    elif m1==1 and m2==3:
-        d=((tpl2[2]*366) + (tpl2[1]*30) + tpl2[0]) - ((tpl1[2]*366) + (tpl1[1]*28) + tpl1[0])
+d1 = date(tpl1[2], tpl1[1], tpl1[0])
+d2 = date(tpl2[2], tpl2[1], tpl2[0])
 
-print('\nNo. of days between the two dates are:', abs(d))
+d = abs((d2 - d1).days)
+
+print("\nNo. of days between the two dates are:", d)
