f6f0f4a5a0f604d00b60fc926c6f677ac8dfaf09
Marhi03 <marhi.patel@gmail.com>
2026-03-14 10:53:44 +0530

    Update Str-5.py

--- Str-5.py
+++ Str-5.py
@@ -1,3 +1,6 @@
+#Given the string s = "PDEU College", write a program to extract the following:
+#"PDEU", "College", "U Col", The complete string in reverse order
+
 s = 'PDEU College'
 print(s[0:4])
 print(s[5:])
