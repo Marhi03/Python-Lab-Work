1e1ed9ce5749839979ddb868b12b652c2b880187
Marhi03 <marhi.patel@gmail.com>
2026-03-14 10:59:04 +0530

    Update Str A-2.py

--- Str A-2.py
+++ Str A-2.py
@@ -1,3 +1,6 @@
+#Write a function that removes a specified substring from a given string if it exists.
+#Example: onestring = "abcdef", removestring = "cd", The resulting string should be "abef".
+
 s = input('Enter a string: ')
 sr = input('Enter the string to be removed: ')
 if (sr in s):
