af3fc6b08a81edf59ebd0ca3eb1cd1ea4d5e7cb1
Marhi03 <marhi.patel@gmail.com>
2026-03-14 10:54:28 +0530

    Update Str-6.py

--- Str-6.py
+++ Str-6.py
@@ -1,3 +1,6 @@
+#Write a program that accepts two strings from the user, concatenates them, 
+#and then repeats the concatenated string n times, where n is entered by the user.
+
 s1 = input('Enter a string: ')
 s2 = input('Enter another string: ')
 s3=' '
