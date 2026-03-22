b83d633c061866caea842c1862f04c9a5a2738d4
Marhi03 <marhi.patel@gmail.com>
2026-03-22 10:33:58 +0530

    Update Set-1.py

--- Set-1.py
+++ Set-1.py
@@ -1,3 +1,9 @@
+#Two students choose subjects represented as sets:
+#s1 = {'Math', 'Physics', 'Chemistry'} and s2 = {'Physics', 'Biology', 'Math'}
+#Write a program to:
+#Find common subjects. Find subjects taken by only the first student. Find subjects taken by only the second student.
+#Find total unique subjects.
+
 s1={'Math', 'Physics', 'Chemistry'}
 s2={'Physics', 'Biology', 'Math'}
 print('Common subjects are:', s1.intersection(s2))
