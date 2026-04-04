b1446a8bac7358317da7471d031ff6e19941e07d
Marhi03 <marhi.patel@gmail.com>
2026-04-04 19:04:04 +0530

    Update Func-6.py

--- Func-6.py
+++ Func-6.py
@@ -1,3 +1,6 @@
+#Write a function to create and return a list containing tuples of the 
+#form (x,x²,x³) for all x between 1 and given ending value (both inclusive).
+
 def function():
     n=int(input('Enter a no.: '))
     lst=[]
