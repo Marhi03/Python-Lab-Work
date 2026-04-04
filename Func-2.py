c7398df565f36023e1630bc61c0dd107d414757f
Marhi03 <marhi.patel@gmail.com>
2026-04-04 19:03:06 +0530

    Update Func-2.py

--- Func-2.py
+++ Func-2.py
@@ -1,3 +1,6 @@
+#Write a program that defines a function compute() that calculates the value 
+#of n + nn + nnn + nnnn, where n is digit received by the function. test the function for digits 4 to 7.
+
 def compute():
     n=int(input('Enter a digit: '))
     s=0
